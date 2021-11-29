37fd5c25dc5a9beed61d30dc9e7c7341c8d2a39c
Matija Kejžar <matija@kejzar.si>
2021-11-29 22:33:32 +0100

    Dotted line attempt 1.

--- freehandTool.js
+++ freehandTool.js
@@ -28,7 +28,7 @@ function FreehandTool() {
                 //if we already have values for previousX and Y we can draw a line from
             //there to the current mouse location
             else {
-                line(previousMouseX, previousMouseY, mouseX, mouseY);
+                drawLine();
                 previousMouseX = mouseX;
                 previousMouseY = mouseY;
             }
@@ -42,23 +42,23 @@ function FreehandTool() {
         }
     };
 
-    this.unselectTool = function() {
+    this.unselectTool = function () {
         options = null;
         select(".options").html("");
         strokeWeight(1);
         colourPalette.alpha(255);
     };
 
-    this.populateOptions = function() {
+    this.populateOptions = function () {
         options = new FreehandOptions();
         options.createUi(select(".options"));
 
-        options.onLineThicknessChanged(function(lineThickness) {
+        options.onLineThicknessChanged(function (lineThickness) {
             console.log("Line thickness changed to", lineThickness, "px.");
             strokeWeight(lineThickness);
         })
 
-        options.onOpacityChanged(function(alpha) {
+        options.onOpacityChanged(function (alpha) {
             var mappedAlpha = Math.round(map(alpha, 0.0, 1.0, 0, 255));
             console.log(`Alpha ${alpha} mapped to range 0-255: ${mappedAlpha}.`);
             colourPalette.alpha(mappedAlpha);
@@ -69,4 +69,28 @@ function FreehandTool() {
         console.log("Freehand tool received colour palette ", palette, ".");
         colourPalette = palette;
     }
+
+    var drawLine = function () {
+        if (options.lineType === 'Solid') {
+            line(previousMouseX, previousMouseY, mouseX, mouseY);
+        }
+        else {
+            var d = dist(previousMouseX, previousMouseY, mouseX, mouseY);
+            console.log(`pmx, pmy, mouseX, mouseY, distance: ${previousMouseX}, ${previousMouseY}, ${mouseX}, ${mouseY}, ${d}`);
+
+            if (d > 0 && options.lineType === 'Dotted') {
+                var xDiff = abs(mouseX - previousMouseX);
+                var yDiff = abs(mouseY - previousMouseY);
+                for (var p = 0; p <= d; p++) {
+                    var x = lerp(previousMouseX, mouseX, p);
+                    var y = lerp(previousMouseY, mouseY, p);
+                    console.log(`(${x}, ${y}) at ${p}`);
+                    // if (p % 2 === 0) {
+                    //     point(x,y);
+                    // }
+                    point(x, y);
+                }
+            }
+        }
+    };
 }
