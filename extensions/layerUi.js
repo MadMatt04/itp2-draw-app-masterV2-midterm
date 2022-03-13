a26c86125bb081fef7e154c5eeca4b393e66b4ae
Matija Kejžar <matija@kejzar.si>
2022-03-13 23:52:04 +0100

    Commented on layerUi and mathHelpers.

--- extensions/layerUi.js
+++ extensions/layerUi.js
@@ -1,12 +1,32 @@
+/**
+ * This constructor function contains builds and manages the UI to support the layer functionality.
+ * @param layerManager The LayerManager this LayerUi operates on.
+ * @constructor Returns a new LayerUi object with the specified settings.
+ */
 function LayerUi(layerManager) {
 
+    /**
+     * The buttons appearing at the bottom of the panel.
+     */
     var layerButtons = [];
+
+    /**
+     * The currently selected layer.
+     */
     var selectedLayer = layerManager.activeLayer();
+
+    /**
+     * The layer panel DOM element
+     */
     var layerPanel;
 
+    /**
+     * Creates the layer panel UI.
+     * @param parent The parent DOM element to attach the layer panel to.
+     */
     this.createUi = function(parent) {
-        console.log("Layer UI parent", parent);
 
+        // Create the sub-panel for the opacity control.
         var opacityPanel = createDiv();
         opacityPanel.addClass("opacityP");
         parent.child(opacityPanel);
@@ -22,11 +42,14 @@ function LayerUi(layerManager) {
                 selectedLayer.alpha(mappedAlpha);
             });
 
+        // The top-level layer panel.
         layerPanel = createDiv();
         layerPanel.addClass("layerP");
         parent.child(layerPanel);
 
         createLayerRows();
+
+        // The next section creates all the buttons for managing the layers.
 
         var addBtn = createImg("../assets/add-svgrepo-com.svg", "New layer");
         addBtn.attribute("title", "New layer");
@@ -58,39 +81,57 @@ function LayerUi(layerManager) {
         parent.child(deleteBtn);
         deleteBtn.mousePressed(deleteLayer);
 
+        // Add the buttons to the list.
         layerButtons.push(addBtn);
         layerButtons.push(upBtn);
         layerButtons.push(downBtn);
         layerButtons.push(renameBtn);
         layerButtons.push(deleteBtn);
 
+        // Set the initial enabled status of the buttons.
         enableOrDisableButtons();
     };
 
+    /**
+     * Create a row in the layer panel for each layer.
+     */
     var createLayerRows = function() {
         layerManager.layers.slice().reverse().forEach(layer => {
             layerPanel.child(createLayerRow(layer));
         });
     }
 
+    /**
+     * Create an element with the name and visibility toggle for the specified layer.
+     * @param layer The layer to create the element for.
+     * @returns A p5.Element representing a layer row.
+     */
     var createLayerRow = function(layer) {
+        // The top-level div.
         var layerRow = createDiv();
         layerRow.addClass("layerRow");
+
+        // Add the selectedLayer class if the layer is selected.
         if (layer === selectedLayer) {
             layerRow.addClass("selectedLayer");
         }
 
+        // Add the eye (visibility toggle) icon.
         var visibilityBtn = createImg("../assets/eye-svgrepo-com-orig.svg", "Layer visibility toggle");
         visibilityBtn.addClass("visibilityBtn");
         visibilityBtn.attribute('width', '25');
         layerRow.child(visibilityBtn);
 
+        // A span containing the name of the layer.
         var span = createSpan(layer.name);
         span.addClass('layerN');
         layerRow.child(span);
 
+        // This flag ensures that the event is consumed if we click on the eye (visibility) icon, so the selection
+        // change is not triggered.
         var eventConsumed = false;
 
+        // Logic to handle the click on the visibility icon. Change from icon to crossed out icon (or vice-versa).
         visibilityBtn.mousePressed(function() {
             layer.toggleVisibility();
             if (layer.visible) {
@@ -102,6 +143,7 @@ function LayerUi(layerManager) {
             eventConsumed = true;
         });
 
+        // Handle click on the layer row that is outside the eye (visibility icon) - this selects the layer.
         layerRow.mousePressed(function() {
             if (!eventConsumed) {
                 selectLayer(layer, layerRow);
@@ -114,9 +156,14 @@ function LayerUi(layerManager) {
         return layerRow;
     };
 
+    /**
+     * Enables or disables buttons, based on the currently selected layer.
+     */
     var enableOrDisableButtons = function() {
         for (var i = 0; i < layerButtons.length; i++) {
+            // Enable Add and Rename button always. Enable other buttons too if not background layer.
             var enable = i === 0 || i === 3 || !selectedLayer.isBackgroundLayer();
+            // Only enable move up and down if it makes sense given the layer's position.
             if (enable && layerManager.layers.length > 1) {
                 if (i === 1) {
                     enable = !layerManager.isTopLayer(selectedLayer);
@@ -126,6 +173,7 @@ function LayerUi(layerManager) {
                 }
             }
 
+            // Add the appropriate classes for enabling and disabling.
             if (enable && layerButtons[i].hasClass("disabled-btn")) {
                 layerButtons[i].removeClass("disabled-btn");
             }
@@ -135,6 +183,9 @@ function LayerUi(layerManager) {
         }
     };
 
+    /**
+     * Add a new layer row.
+     */
     var addLayer = function() {
         var layer = layerManager.createLayer();
         var layerRow = createLayerRow(layer);
@@ -142,6 +193,9 @@ function LayerUi(layerManager) {
         selectLayer(layer, layerRow);
     };
 
+    /**
+     * Deletes a layer. First prompts for confirmation.
+     */
     var deleteLayer = function() {
         if (window.confirm(`Really remove layer '${selectedLayer.name}'?`)) {
             var deletedLayerIndex = layerManager.deleteLayer(selectedLayer);
@@ -156,6 +210,9 @@ function LayerUi(layerManager) {
         }
     };
 
+    /**
+     * Deselects all layers.
+     */
     var deselectLayer = function() {
         if (layerPanel) {
             layerPanel.child().forEach(layerRow => {
@@ -164,23 +221,40 @@ function LayerUi(layerManager) {
         }
     };
 
+    /**
+     * Selects the layer represented by the layer parameter and the specified layerRow.
+     * @param layer The Layer object to select.
+     * @param layerRow The layer row that represents the specified layer.
+     */
     var selectLayer = function(layer, layerRow) {
         deselectLayer();
         selectedLayer = layer;
-        console.log("Selected layer", layer.name);
         layerManager.activeLayer(selectedLayer);
         layerRow.addClass("selectedLayer");
         enableOrDisableButtons();
     };
 
+    /**
+     * Moves the layer up the panel.
+     */
     var moveLayerUp = function() {
         moveLayer(layerManager.moveLayerUp, -1, 0);
     };
 
+    /**
+     * Moves the layer down the panel.
+     */
     var moveLayerDown = function() {
         moveLayer(layerManager.moveLayerDown, 1, -1);
     };
 
+    /**
+     * Move the layer up or down the panel and handle all the UI properly.
+     * @param moveLayerMethod The LayerManager method that handles the logic of moving the layer up or down.
+     * @param offset The direction of movement, which is -1 for up and 1 for down the panel. Note that this is exactly
+     * reverse of how the layers are ordered in LayerManager.
+     * @param insertBeforeOffset The offset used in the calculation of where to insert the moved row.
+     */
     var moveLayer = function(moveLayerMethod, offset, insertBeforeOffset) {
         var layerIndex = moveLayerMethod.call(layerManager, selectedLayer);
         var layerRows = Array.from(layerPanel.child()).map(layerRow => new p5.Element(layerRow)).reverse();
@@ -192,15 +266,22 @@ function LayerUi(layerManager) {
         selectLayer(selectedLayer, movedRow);
     };
 
+    /**
+     * Handles the UI logic for renaming the layer by prompting the user for a new name and then executing the
+     * proper logic.
+     */
     var renameLayer = function() {
         var newName = prompt("Rename layer to", selectedLayer.name);
         selectedLayer.name = newName;
         var layerRow = findSelectedLayerRow();
         var span = layerRow.elt.getElementsByTagName("span")[0];
         span.innerHTML = newName;
-        console.log("SPAN", span);
     };
 
+    /**
+     * Finds and returns the p5.Element DOM element representing the selected layer.
+     * @returns {p5.Element}
+     */
     var findSelectedLayerRow = function() {
         return Array.from(layerPanel.child()).map(layerRow => new p5.Element(layerRow)).find(
             layerRow => layerRow.hasClass("selectedLayer"));
--- extensions/mathHelpers.js
+++ extensions/mathHelpers.js
@@ -1,3 +1,12 @@
+/**
+ * Returns a vector that is perpendicular to a vector described by (x1, y1) and (x2, y2) coordinates.
+ * @param x1 The X coordinate of the first point.
+ * @param y1 The Y coordinate of the first point.
+ * @param x2 The X coordinate of the second point.
+ * @param y2 The Y coordinate of the second point.
+ * @param magnitude The magnitude of the returned vector. 1.0 (unit vector) by default.
+ * @returns {p5.Vector} perpendicular to the original vector.
+ */
 function getPerpendicularVectorP(x1, y1, x2, y2, magnitude=1.0) {
     var v1 = createVector(x1, y1);
     var v2 = createVector(x2, y2);
@@ -6,6 +15,13 @@ function getPerpendicularVectorP(x1, y1, x2, y2, magnitude=1.0) {
 
 }
 
+/**
+ * Returns a vector that is perpendicular to a vector described by the difference of vectors v2 - v1.
+ * @param v1 The first vector.
+ * @param v2 The second vector.
+ * @param magnitude The magnitude of the returned vector. 1.0 (unit vector) by default.
+ * @returns {p5.Vector} perpendicular to the original vector.
+ */
 function getPerpendicularVector(v1, v2, magnitude=1.0) {
     var v = p5.Vector.sub(v2, v1);
 
@@ -15,18 +31,11 @@ function getPerpendicularVector(v1, v2, magnitude=1.0) {
     return vp;
 }
 
+/**
+ * Returns a random boolean value.
+ * @returns {boolean} True or false, randomly.
+ */
 function randomBoolean() {
     var r = round(random(0, 1));
     return r >= 0.5;
-}
-
-function testPerpVector() {
-    var vp = getPerpendicularVectorP(1, 1, 2, 3, 5);
-    var v1 = createVector(1, 1);
-    var v2 = createVector(2, 3);
-    var v = p5.Vector.sub(v2, v1);
-
-    console.assert(p5.Vector.dot(v, vp) === 0);
-
-    console.log("v", v, "vp", vp, "dot", p5.Vector.dot(v, vp));
 }
